792b3d7e868bb1adb4ec9741892a577fdb00048e
asdasde <lukaerceg959@gmail.com>
2024-11-09 17:10:51 +0100

    added some stuff

--- knowledge_graph/UserGraph.py
+++ knowledge_graph/UserGraph.py
@@ -37,46 +37,29 @@ class UserGraph:
         return [load_and_process_image(AVATAR_PATH_TEMPLATE.format(i)) for i in range(1, NUM_AVATARS+1)]
 
     def add_user(self, user_id: Any, attributes: Dict) -> None:
-        """
-        Add a user node to the graph with optional attributes and automatically create edges
-        if teaching and learning subjects match between users.
-
-        Parameters:
-            user_id (Any): Unique identifier for the user.
-            attributes (Dict): Dictionary of user attributes, including 'teaching_subject',
-                               'learning_subject', and 'rating_avg'.
-        """
-        # Add the user as a node in the graph, including all attributes in the `attributes` dictionary
         if user_id in self.graph.nodes.keys():
             return
 
         self.graph.add_node(user_id, **attributes)
 
-        # If the user has a non-NaN 'teaching_subject', create edges for matching 'learning_subject' nodes
         if attributes['teaching_subject'] is not np.nan:
             for x, data in self.graph.nodes(data=True):
-                # Check if any node has a 'learning_subject' matching this user's 'teaching_subject'
                 if data.get('learning_subject') == attributes['teaching_subject']:
                     edge_attrs = {
                         'subject': attributes['teaching_subject'],
                         'rating_avg_teacher': attributes['rating_avg_teacher']
                     }
-                    # Add a directed edge from this user (teacher) to the matching student node
                     self.graph.add_edge(user_id, x, **edge_attrs)
 
-        # If the user has a non-NaN 'learning_subject', create edges for matching 'teaching_subject' nodes
         if attributes['learning_subject'] is not np.nan:
             for x, data in self.graph.nodes(data=True):
-                # Check if any node has a 'teaching_subject' matching this user's 'learning_subject'
                 if data.get('teaching_subject') == attributes['learning_subject']:
                     edge_attrs = {
                         'subject': data['teaching_subject'],
                         'rating_avg_teacher': data['rating_avg_teacher']
                     }
-                    # Add a directed edge from the matching teacher node to this user (student)
                     self.graph.add_edge(x, user_id, **edge_attrs)
 
-        # Update the cycles and nodes involved in cycles after adding the new user and edges
         self._update()
 
     def remove_user(self, user_id: Any) -> None:
@@ -85,16 +68,6 @@ class UserGraph:
             self._update()
 
     def find_best_cycle(self, user_id: Any) -> Optional[list]:
-        """
-        Find the best cycle (shortest or most favorable path) starting and ending at a given user.
-
-        Parameters:
-            user_id (Any): Unique identifier for the user.
-            weight (Optional[str]): Edge attribute to use as weight for cycle calculation.
-
-        Returns:
-            Optional[list]: List of nodes forming the best cycle for the user, or None if no cycle is found.
-        """
         if user_id not in self.graph:
             return None
 
@@ -117,13 +90,6 @@ class UserGraph:
     def build_from_dataframe(self,
                              users_df: pd.DataFrame,
                              edges_df: pd.DataFrame) -> None:
-        """
-        Build the graph from DataFrames for users and edges.
-
-        Parameters:
-            users_df (pd.DataFrame): DataFrame containing user information.
-            edges_df (pd.DataFrame): DataFrame containing edges information.
-        """
 
         source_col = 'user_id_teacher'
         target_col = 'user_id_student'
@@ -138,11 +104,17 @@ class UserGraph:
         self.graph = G
 
 
-    def draw_cycle(self, user_id : int):
+    def draw_cycle(self, user_id : int, connection_style = 'arc3,rad=0.1'):
+        
+        
+        best_cycle = self.find_best_cycle(user_id)
 
-        G = nx.subgraph(self.graph, self.find_best_cycle(user_id))
+        if best_cycle is None:
+            return
 
-        pos = nx.spring_layout(G)
+        G = nx.subgraph(self.graph, best_cycle)
+
+        pos = nx.arf_layout(G, pos = nx.planar_layout(G))
 
         teaching_subjects = set(nx.get_node_attributes(G, 'teaching_subject').values())
         color_map = {subject: f'#{random.randint(0, 0xFFFFFF):06x}' for subject in teaching_subjects}
@@ -154,7 +126,7 @@ class UserGraph:
 
         nx.draw(
             G, pos, node_size=0, edge_color='black',
-            arrows=True, arrowsize=1, connectionstyle='angle3',
+            arrows=True, arrowsize=1, connectionstyle=connection_style,
         )
 
         for edge in G.edges():
@@ -165,7 +137,6 @@ class UserGraph:
             start_subject = G.nodes[source]['teaching_subject']
             edge_color = color_map[start_subject]  # Use the start node's teaching subject for color
 
-            # Draw each edge individually
             nx.draw_networkx_edges(
                 G, pos,
                 edgelist=[(source, target)],
@@ -173,19 +144,15 @@ class UserGraph:
                 arrowsize=arrowsize,
                 edge_color=edge_color,
                 min_target_margin=20,
-                connectionstyle="angle3"
+                connectionstyle=connection_style
             )
             nx.draw_networkx_edge_labels(
                 G, pos,
                 edge_labels=edge_labels,
-                connectionstyle="angle3",
+                connectionstyle=connection_style,
                 font_family='monospace'
             )
             
-        #     nx.draw(G, pos=pos, with_labels=1, node_color='red',
-        # node_size=3000, font_color='white', font_size=20,
-        # font_family='Times New Roman', font_weight='bold',
-        # width=5)
         ax = plt.gca()
         for node, (x, y) in pos.items():
             print(node, node % NUM_AVATARS)
@@ -200,25 +167,10 @@ class UserGraph:
         plt.show()
 
     def save_to_file(self, file_path: Union[str, os.PathLike]) -> None:
-        """
-        Serializes and saves the UserGraph instance to a pickle file.
-
-        Parameters:
-            file_path (Union[str, os.PathLike]): Path to the file where the instance will be saved.
-        """
         with open(file_path, 'wb') as f:
             pickle.dump(self, f)
 
     @classmethod
     def load_from_file(cls, file_path: Union[str, os.PathLike]) -> 'UserGraph':
-        """
-        Loads a UserGraph instance from a pickle file.
-
-        Parameters:
-            file_path (Union[str, os.PathLike]): Path to the file where the instance is saved.
-
-        Returns:
-            UserGraph: The deserialized UserGraph instance.
-        """
         with open(file_path, 'rb') as f:
             return pickle.load(f)
